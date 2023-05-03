e2ca07b642b4004af31dc3608dad866a04964aa2
Sasa Prsic <sasa@ethernal.tech>
2023-05-03 09:12:02 +0200

    Add retry.

--- chains/nomad-substrate/src/client.rs
+++ chains/nomad-substrate/src/client.rs
@@ -1,15 +1,17 @@
-
 use crate::SubstrateError;
 use color_eyre::Result;
 use ethers_core::types::Signature;
 use nomad_core::{RawCommittedMessage, SignedUpdate, SignedUpdateWithMeta, Update, UpdateMeta};
 use std::convert::TryInto;
+use std::time::Duration;
+use async_std::task::sleep;
 use subxt::ext::sp_runtime::traits::Header;
-use subxt::{
-    dynamic::Value, ext::scale_value::scale::TypeId, storage::DynamicStorageAddress, Config,
-    OnlineClient,
-};
+use subxt::{dynamic::Value, ext::scale_value::scale::TypeId, storage::DynamicStorageAddress, Config, OnlineClient, Error};
 use avail_subxt::api::nomad_home as home;
+use subxt::error::MetadataError::ErrorNotFound;
+use subxt::error::RpcError;
+use tracing::{error, info};
+use nomad_core::NomadError::IoError;
 
 /// Nomad wrapper around `subxt::OnlineClient`
 #[derive(Clone)]
@@ -27,8 +29,8 @@ impl<T: Config> std::ops::Deref for NomadOnlineClient<T> {
 }
 
 impl<T: Config> NomadOnlineClient<T>
-where
-    <T as Config>::BlockNumber: TryInto<u32>,
+    where
+        <T as Config>::BlockNumber: TryInto<u32>,
 {
     /// Instantiate a new NomadOnlineClient
     pub fn new(client: OnlineClient<T>, timelag: Option<u8>) -> Self {
@@ -37,11 +39,25 @@ where
 
     /// Get most recent block number
     pub async fn get_block_number(&self) -> Result<u32, SubstrateError> {
-        let header = self.rpc().header(None).await?.unwrap();
-        let u32_header = (*header.number()).try_into();
+        let mut retry_count = 0;
+        loop {
+            match self.rpc().header(None).await {
+                Ok(header) => {
+                    let u32_header: Result<u32, SubstrateError> = (*header.unwrap().number()).try_into();
 
-        u32_header
-            .map_err(|_| SubstrateError::CustomError("Couldn't convert block number to u32".into()))
+                    return u32_header
+                        .map_err(|_| SubstrateError::CustomError("Couldn't convert block number to u32".into()));
+                }
+                Err(err) => {
+                    retry_count += 1;
+                    if retry_count == 5 {
+                        return Err(SubstrateError::from(err));
+                    }
+                }
+            };
+            info!("Retrying {} header", retry_count);
+            sleep(Duration::from_millis(300)).await;
+        }
     }
 
     /// Fetch value from storage with built-in timelag
@@ -54,12 +70,9 @@ where
             .timelag
             .map_or(block_number, |lag| block_number - lag as u32);
 
-        let opt_block_hash = self
-            .rpc()
-            .block_hash(Some(final_block_number.into()))
-            .await?;
+        let opt_block_hash = self.get_block_hash(final_block_number.into()).await?;
 
-        Ok(self.storage().fetch(address, opt_block_hash).await?)
+        Ok(self.storage().fetch(address, Some(opt_block_hash)).await?)
     }
 
     /// Fetch ordered signed updates from the specific `block_number`
@@ -68,11 +81,7 @@ where
         block_number: u32,
     ) -> Result<Vec<SignedUpdateWithMeta>, SubstrateError> {
         // Get hash for block number
-        let hash = self
-            .rpc()
-            .block_hash(Some(block_number.into()))
-            .await?
-            .unwrap();
+        let hash = self.get_block_hash(block_number.into()).await?;
 
         // Get updates from block
         let update_events_res: Result<Vec<_>, _> = self
@@ -118,11 +127,8 @@ where
         block_number: u32,
     ) -> Result<Vec<RawCommittedMessage>, SubstrateError> {
         // Get hash for block number
-        let hash = self
-            .rpc()
-            .block_hash(Some(block_number.into()))
-            .await?
-            .unwrap();
+
+        let hash = self.get_block_hash(block_number.into()).await?;
 
         // Get dispatch events from block
         let dispatch_events_res: Result<Vec<_>, _> = self
@@ -146,5 +152,24 @@ where
                 message: ev.message,
             })
             .collect())
+    }
+
+    async fn get_block_hash(&self, block_number: u32) -> Result<<T as Config>::Hash, SubstrateError> {
+        let mut retry_count = 0;
+        loop {
+            match self.rpc().block_hash(Some(block_number.into())).await {
+                Ok(res) => {
+                    return Ok(res.unwrap());
+                }
+                Err(err) => {
+                    retry_count += 1;
+                    if retry_count == 5 {
+                        return Err(SubstrateError::from(err));
+                    }
+                }
+            };
+            info!("Retrying {} get_block_hash", retry_count);
+            sleep(Duration::from_millis(300)).await;
+        }
     }
 }
